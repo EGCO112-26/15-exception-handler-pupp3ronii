1801b265b91358beb398d75bfb3931b1ea2caa4f
pupp3ronii <pakkapong55059@gmail.com>
2026-04-20 08:31:17 +0000

    catch

--- main.cpp
+++ main.cpp
@@ -1,18 +1,59 @@
+#include <iostream>
+using namespace std;
+
+void check0(int x){
+	if(x==0) throw(x);
+}
+
 int main(){
-	int x,y;
+	int x,y,a;
 	double d;
-	cin>>x>>y;
-	if(cin.fail()){
-		cerr<<"Incorrect type entered"; return 1;
-	}
-	if(abs(x)>1000 ||abs(y)>1000){
-		cerr<<"alue out of range"; return 1;
-	}
-	if (y==0) {
-		cerr<< "Error divide by zero"; return 1;
+do{
+	a=0;
+try{
+	int i;
+	double* myarray;
+	for(i=0; i<10000000; i++){
+		cout<<"Allocating memory..."<<i<<endl;
+		myarray = new double(500000000);
 	}
 
+	cout<<"Enter 2 numbers";
+	cin>>x>>y;
+
+	if(cin.fail()){
+		throw("Incorrect type entered");
+
+	}
+	if(abs(x)>1000 ||abs(y)>1000){
+		throw("Value out of range");
+	
+	}
+	check0(y);
+	if (y==0) {
+		throw("Error divide by zero");
+
+	}
+
+	/*normal code*/
 	d=(double) x/y;
-	cout<< "The result is" <<d;
+	cout<< "The result is" <<d<<endl;
+}
+catch(const char* error){
+	cerr<<error<<endl;
+	a=1;
+	cin.clear();
+	cin.ignore(50, '\n');
+}
+catch(int e){
+	switch(e){
+		case 101: cerr<<"Incorrect numbers "<<endl; break;
+		case 0: cerr<<"Divided by zero"<<endl; break;
+	}
+}
+catch(...){
+	cout<<"Memory leak!!"<<endl;
+}
+}while(a);
 	return 0;
 }
